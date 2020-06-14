a03555f01f11ea851cf5241b5e675261d344ff0a
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-06-14 16:15:28 +0930

    Implemented Scss
    - Added node-sass
    - Clean up css and index

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import { Header } from './Components/Header';
 import { Hero } from './Components/Hero';
 import { About } from './Components/About';
@@ -8,7 +8,7 @@ import { Portfolio } from './Components/Portfolio';
 import { Education } from './Components/Education';
 import { Footer } from './Components/Footer';
 
-function App() {
+export const App = () => {
   return (
     <div className='App'>
       <Header />
@@ -20,6 +20,6 @@ function App() {
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
