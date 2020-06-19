dff9a03146fdc9943254c96642a6ff8c511c110b
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-06-19 12:10:58 +0930

    Added SocialIcons and Copyright components

---
+++ src/UI/Copyright.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface ICopyright {
+  name: string;
+  twitterUrl?: string;
+  twitterName?: string;
+}
+
+export default function Copyright(ICopyright: ICopyright) {
+  return (
+    <p className='text-sm text-gray-600 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4'>
+      © 2020 {ICopyright.name} —
+      <a
+        href={ICopyright.twitterUrl}
+        className='text-gray-500 ml-1'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        {ICopyright.twitterName}
+      </a>
+    </p>
+  );
+}
---
+++ src/UI/SocialIcons.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+
+interface ISocialIcons {
+  facebookURL?: string;
+  twitterURL?: string;
+  instagramURL?: string;
+  linkedInURL?: string;
+}
+
+export default function SocialIcons(ISocialIcons: ISocialIcons) {
+  return (
+    <span className='inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start'>
+      {ISocialIcons.facebookURL && (
+        <a
+          className='text-gray-600'
+          href={ISocialIcons.facebookURL}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <svg
+            fill='currentColor'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-width='2'
+            className='w-5 h-5'
+            viewBox='0 0 24 24'
+          >
+            <path d='M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z'></path>
+          </svg>
+        </a>
+      )}
+      {ISocialIcons.twitterURL && (
+        <a
+          className='ml-3 text-gray-600'
+          href={ISocialIcons.twitterURL}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <svg
+            fill='currentColor'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-width='2'
+            className='w-5 h-5'
+            viewBox='0 0 24 24'
+          >
+            <path d='M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z'></path>
+          </svg>
+        </a>
+      )}
+      {ISocialIcons.instagramURL && (
+        <a
+          className='ml-3 text-gray-600'
+          href={ISocialIcons.instagramURL}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <svg
+            fill='none'
+            stroke='currentColor'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-width='2'
+            className='w-5 h-5'
+            viewBox='0 0 24 24'
+          >
+            <rect width='20' height='20' x='2' y='2' rx='5' ry='5'></rect>
+            <path d='M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01'></path>
+          </svg>
+        </a>
+      )}
+      {ISocialIcons.linkedInURL && (
+        <a
+          className='ml-3 text-gray-600'
+          href={ISocialIcons.linkedInURL}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <svg
+            fill='currentColor'
+            stroke='currentColor'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-width='0'
+            className='w-5 h-5'
+            viewBox='0 0 24 24'
+          >
+            <path
+              stroke='none'
+              d='M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z'
+            ></path>
+            <circle cx='4' cy='4' r='2' stroke='none'></circle>
+          </svg>
+        </a>
+      )}
+    </span>
+  );
+}
