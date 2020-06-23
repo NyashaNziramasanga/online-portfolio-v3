f90e1111406fa1a88f6174793e11e118af7c5292
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-06-24 01:08:19 +0930

    Updated scripts and copyright year

--- src/UI/Copyright.tsx
+++ src/UI/Copyright.tsx
@@ -9,7 +9,7 @@ interface ICopyright {
 export default function Copyright(ICopyright: ICopyright) {
   return (
     <p className='text-sm text-gray-600 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4'>
-      © 2020 {ICopyright.name} —
+      © {new Date().getFullYear()} {ICopyright.name} —
       <a
         href={ICopyright.twitterUrl}
         className='text-gray-500 ml-1'
