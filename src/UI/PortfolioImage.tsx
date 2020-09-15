043e5a3a984bb47bc6e2b72786e4297a1ab2ebf1
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-09-15 17:25:27 +0930

    fix: fixed image hover display

--- src/UI/PortfolioImage.tsx
+++ src/UI/PortfolioImage.tsx
@@ -10,12 +10,23 @@ export default function PortfolioImage(props: IPortfolio) {
       rel='noopener noreferrer'
     >
       <div className='sm:w-64 sm:h-40 h-64 w-40 sm:mr-5 inline-flex items-center justify-center text-blue-400 flex-shrink-0 rounded'>
-        <img
-          className='hover:opacity-0 bg-white absolute sm:w-64 sm:h-40 transition ease-in duration-500'
-          src={props.image}
-          alt={props.image}
-        />
-        <img className='sm:w-64 sm:h-40' src={props.gif} alt={props.gif} />
+        {props.gif ? (
+          <img
+            className='hover:opacity-0 bg-white absolute sm:w-64 sm:h-40 transition ease-in duration-500'
+            src={props.image}
+            alt={props.image}
+          />
+        ) : (
+          <img
+            className='bg-white absolute sm:w-64 sm:h-40'
+            src={props.image}
+            alt={props.image}
+          />
+        )}
+
+        {props.gif && (
+          <img className='sm:w-64 sm:h-40' src={props.gif} alt={props.gif} />
+        )}
       </div>
     </a>
   );
